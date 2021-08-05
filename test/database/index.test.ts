9e43a75933d997e0d390fbc10d4575b44b4438b1
Kieran Mesquita <mesquitakieran@gmail.com>
2021-08-05 16:23:23 +1000

    Full code coverage

--- test/database/index.test.ts
+++ test/database/index.test.ts
@@ -1,15 +1,19 @@
 /* globals describe it beforeEach afterEach */
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
+
 import * as rimraf from 'rimraf';
+import * as level from 'level';
 
 import Database from '../../src/database';
 
 chai.use(chaiAsPromised);
 const { expect } = chai;
 
+// Database path
 const testDBPath = './test/testDB';
 
+// Database object for tests
 let db: Database;
 
 describe('Database', () => {
@@ -26,8 +30,24 @@ describe('Database', () => {
     expect(await db.level.get('a')).to.equal('1');
   });
 
+  it('Should create database from existing level object', async () => {
+    // Create database from level object
+    const db2: Database = new Database(level(`${testDBPath}2`));
+
+    // Put value in database
+    await db2.level.put('a', '1');
+
+    // Check if value is returned correctly
+    expect(await db2.level.get('a')).to.equal('1');
+
+    // Clean up database
+    db2.level.close();
+    rimraf(`${testDBPath}2`, () => {});
+  });
+
   afterEach(() => {
     // Clean up database
+    db.level.close();
     rimraf(testDBPath, () => {});
   });
 });
