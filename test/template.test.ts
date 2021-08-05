2775e270a527271699a72d0509d41ef0ae264535
Kieran Mesquita <mesquitakieran@gmail.com>
2021-08-05 16:11:09 +1000

    Test and code coverage reporting

--- test/template.test.ts
+++ test/template.test.ts
@@ -1,6 +1,8 @@
 /* globals describe it */
 import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
 
+chai.use(chaiAsPromised);
 const { expect } = chai;
 
 describe('Test Template', () => {
