a77e95bbebe4e8516ee9fd8e79e8b2f1787f407e
jrmeurer <jrmeurer@wisc.edu>
2022-03-18 13:14:50 -0600

    Update Adapt module formatting

--- src/contract/erc20/adapt.ts
+++ src/contract/erc20/adapt.ts
@@ -1,99 +1,140 @@
-import { Contract, PopulatedTransaction, BigNumber, ethers, Overrides, PayableOverrides, CallOverrides} from 'ethers';
+import { Contract, PopulatedTransaction, BigNumber, CallOverrides } from 'ethers';
 import type { Provider } from '@ethersproject/abstract-provider';
-import { bytes} from '../../utils';
-import {
-  ERC20TransactionSerialized,
-} from '../../transaction/erc20';
+import { ERC20TransactionSerialized } from '../../transaction/erc20';
 import { abi } from './abi';
 import { LeptonDebugger } from '../../models/types';
-import {BytesData} from '../../utils/bytes';
 
-class adapt {
+class Adapt {
   contract: Contract;
 
   // Contract address
   address: string;
-  
+
   readonly leptonDebugger: LeptonDebugger | undefined;
-  
+
   /**
-  * Connect to Railgun instance on network
-  * @param address - address of Railgun instance (Proxy contract)
-  * @param provider - Network provider
-  */
+   * Connect to Railgun instance on network
+   * @param address - address of Railgun instance (Proxy contract)
+   * @param provider - Network provider
+   */
   constructor(address: string, provider: Provider, leptonDebugger?: LeptonDebugger) {
     this.address = address;
     this.contract = new Contract(address, abi, provider);
     this.leptonDebugger = leptonDebugger;
-  }  
-
-  /**
-  *                          
-  * @param
-  * @returns
-  */
-  relay(transactions: ERC20TransactionSerialized[], random: BigNumber, requireSucccess: Boolean, calls:PopulatedTransaction[], overrides?: CallOverrides): Promise<PopulatedTransaction> {
-
-    return this.contract.populateTransaction.relay(transactions,random,requireSucccess,calls.map((call) => {
-      if (!call.to) {
-        throw new Error('Must specify to address');
-      }
-    
-      return {
-        to: call.to,
-        data: call.data || '',
-        value: call.value || '0'
-      }
-    }), overrides);
   }
 
   /**
-  * ERC20TransactionSerialized proof fields should be set to '0'
-  */
-  estimateGas(transactions: ERC20TransactionSerialized[], random: BigNumber, requireSucccess: Boolean, calls:PopulatedTransaction[], overrides?: CallOverrides): Promise<BigNumber> {
+   *
+   * @param
+   * @returns
+   */
+  relay(
+    transactions: ERC20TransactionSerialized[],
+    random: BigNumber,
+    requireSuccess: boolean,
+    calls: PopulatedTransaction[],
+    overrides?: CallOverrides,
+  ): Promise<PopulatedTransaction> {
+    return this.contract.populateTransaction.relay(
+      transactions,
+      random,
+      requireSuccess,
+      calls.map((call) => {
+        if (!call.to) {
+          throw new Error('Must specify to address');
+        }
 
-    const overridesFormatted = overrides || {}
-    overridesFormatted.from = "0x0000000000000000000000000000000000000000";
-    
-    return this.contract.estimateGas.relay(transactions,random,requireSucccess,calls.map((call) => {
-      if (!call.to) {
-        throw new Error('Must specify to address');
-      }
-    
-      return {
-        to: call.to,
-        data: call.data || '',
-        value: call.value || '0'
-      }
-    }), overridesFormatted);
+        return {
+          to: call.to,
+          data: call.data || '',
+          value: call.value || '0',
+        };
+      }),
+      overrides,
+    );
   }
 
-  depositEth(amount:BigNumber, random:BigNumber, wethAddress: String, pubKey: String[]): Promise<PopulatedTransaction> {
+  /**
+   * ERC20TransactionSerialized proof fields should be set to '0'
+   */
+  estimateGas(
+    transactions: ERC20TransactionSerialized[],
+    random: BigNumber,
+    requireSuccess: boolean,
+    calls: PopulatedTransaction[],
+    overrides?: CallOverrides,
+  ): Promise<BigNumber> {
+    const overridesFormatted = overrides || {};
+    overridesFormatted.from = '0x0000000000000000000000000000000000000000';
 
-    const calls = [this.contract.interface.encodeFunctionData("wrapAllEth"), 
-    this.contract.interface.encodeFunctionData("deposit", [[wethAddress], random, pubKey])];
-    
-    return this.relay([], random, true, calls.map((call) => {
-      return {
+    return this.contract.estimateGas.relay(
+      transactions,
+      random,
+      requireSuccess,
+      calls.map((call) => {
+        if (!call.to) {
+          throw new Error('Must specify to address');
+        }
+
+        return {
+          to: call.to,
+          data: call.data || '',
+          value: call.value || '0',
+        };
+      }),
+      overridesFormatted,
+    );
+  }
+
+  depositEth(
+    amount: BigNumber,
+    random: BigNumber,
+    wethAddress: String,
+    pubKey: String[],
+  ): Promise<PopulatedTransaction> {
+    const calls = [
+      this.contract.interface.encodeFunctionData('wrapAllEth'),
+      this.contract.interface.encodeFunctionData('deposit', [[wethAddress], random, pubKey]),
+    ];
+
+    const requireSuccess = true;
+
+    return this.relay(
+      [],
+      random,
+      requireSuccess,
+      calls.map((call) => ({
         to: this.contract.address,
         data: call,
-      }
-    }), {value : amount});
+      })),
+      { value: amount },
+    );
   }
 
-  withdrawEth(transactions: ERC20TransactionSerialized[], random:BigNumber, to:String): Promise<PopulatedTransaction> {
+  withdrawEth(
+    transactions: ERC20TransactionSerialized[],
+    random: BigNumber,
+    to: String,
+  ): Promise<PopulatedTransaction> {
+    const calls = [
+      this.contract.interface.encodeFunctionData('unWrapEth'),
+      this.contract.interface.encodeFunctionData('send', [
+        ['0x0000000000000000000000000000000000000000'],
+        to,
+      ]),
+    ];
 
-    const calls = [this.contract.interface.encodeFunctionData("unWrapEth"), 
-    this.contract.interface.encodeFunctionData("send", [["0x0000000000000000000000000000000000000000"], to])];
-    
-    return this.relay([], random, true, calls.map((call) => {
-      return {
+    const requireSuccess = true;
+
+    return this.relay(
+      [],
+      random,
+      requireSuccess,
+      calls.map((call) => ({
         to: this.contract.address,
         data: call,
-      }
-    }));
+      })),
+    );
   }
-
-
 }
-export {adapt}
+export { Adapt };
