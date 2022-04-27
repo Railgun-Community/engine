607dc2bb0a01dd2f62e81ad5f83f823c9a22f9f3
jrmeurer <jrmeurer@wisc.edu>
2022-04-27 15:12:07 -0600

    Export ecies

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,6 @@ import * as constants from './constants';
 import * as encryption from './encryption';
 import * as hash from './hash';
 import * as keysUtils from './keys-utils';
+import * as ecies from './ecies';
 
-export { bytes, constants, encryption, hash, keysUtils };
+export { bytes, constants, encryption, hash, keysUtils, ecies };
