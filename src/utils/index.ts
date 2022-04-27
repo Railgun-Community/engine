b3c9968515c504c0c29c46a13cb6dfb7d53a2b09
Dylan Oliver <dylan.oliver@gmail.com>
2022-04-27 10:33:08 -0400

    rm utils/ed25519

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,6 @@ import * as bytes from './bytes';
 import * as constants from './constants';
 import * as encryption from './encryption';
 import * as hash from './hash';
-import * as ed25519 from './ed25519';
 import * as keysUtils from './keys-utils';
 
-export { bytes, constants, encryption, hash, ed25519, keysUtils };
+export { bytes, constants, encryption, hash, keysUtils };
