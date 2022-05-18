b249d37d101f8900ed31103fd2efe5b12d6e0e71
jrmeurer <jrmeurer@wisc.edu>
2022-05-18 08:42:09 -0600

    Add tests for next solution batch

--- test/solutions/complex-solutions.test.ts
+++ test/solutions/complex-solutions.test.ts
@@ -2,10 +2,25 @@
 /* globals describe it */
 
 import { expect } from 'chai';
+import { Lepton, Note } from '../../src';
+import { AddressData } from '../../src/keyderivation/bech32-encode';
 import {
+  findNextSolutionBatch,
   nextNullifierTarget,
   shouldAddMoreUTXOsForSolutionBatch,
 } from '../../src/solutions/complex-solutions';
+import { sortUTXOsBySize } from '../../src/solutions/utxos';
+import { bytes } from '../../src/utils';
+import { TreeBalance, TXO } from '../../src/wallet';
+
+const addressData = Lepton.decodeAddress(
+  '0zk1qyqqqqdl645pcpreh6dga7xa3w4dm9c3tzv6ntesk0fy2kzr476pkunpd9kxwatw8qqqqqdl645pcpreh6dga7xa3w4dm9c3tzv6ntesk0fy2kzr476pkcsu8tp',
+);
+
+const createMockTXO = (txid: string, value: bigint): TXO => {
+  const note = new Note(addressData, bytes.random(16), value, 'abc');
+  return { txid, note } as TXO;
+};
 
 describe('Solutions/Complex Solutions', () => {
   it('Should get valid next nullifier targets', () => {
@@ -41,5 +56,49 @@ describe('Solutions/Complex Solutions', () => {
 
     // Lower than total amount. Valid nullifier amount. Next is not reachable. [ALL SET]
     expect(shouldAddMoreUTXOsForSolutionBatch(8, 10, lowAmount, totalRequired)).to.equal(false);
+  });
+
+  it('Should create next solution batch from utxos', () => {
+    const treeBalance1: TreeBalance = {
+      balance: BigInt(150),
+      utxos: [
+        createMockTXO('a', BigInt(30)),
+        createMockTXO('b', BigInt(40)),
+        createMockTXO('c', BigInt(50)),
+        createMockTXO('d', BigInt(10)),
+        createMockTXO('e', BigInt(20)),
+      ],
+    };
+
+    const utxosForSort = [...treeBalance1.utxos];
+    expect(utxosForSort.map((utxo) => utxo.txid)).to.deep.equal(['a', 'b', 'c', 'd', 'e']);
+    sortUTXOsBySize(utxosForSort);
+    expect(utxosForSort.map((utxo) => utxo.txid)).to.deep.equal(['c', 'b', 'a', 'e', 'd']);
+
+    // More than required. No excluded txids.
+    const solutionBatch1 = findNextSolutionBatch(treeBalance1, BigInt(180), []);
+    expect(solutionBatch1.map((utxo) => utxo.txid)).to.deep.equal(['c', 'b']);
+
+    // More than required. Exclude txids.
+    const solutionBatch2 = findNextSolutionBatch(treeBalance1, BigInt(180), ['a', 'b']);
+    expect(solutionBatch2.map((utxo) => utxo.txid)).to.deep.equal(['c', 'e']);
+
+    // Less than required. Exclude txids.
+    const solutionBatch3 = findNextSolutionBatch(treeBalance1, BigInt(10), ['a', 'b']);
+    expect(solutionBatch3.map((utxo) => utxo.txid)).to.deep.equal(['c']);
+
+    // Less than required. Exact match would be 4 UTXOs, which is not an allowed Nullifer count. Most optimal would be b + c.
+    const solutionBatch4 = findNextSolutionBatch(treeBalance1, BigInt(120), []);
+    expect(solutionBatch4.map((utxo) => utxo.txid)).to.deep.equal(['c', 'b']);
+
+    // No utxos available.
+    const solutionBatch5 = findNextSolutionBatch(treeBalance1, BigInt(120), [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+    ]);
+    expect(solutionBatch5).to.equal(undefined);
   });
 });
