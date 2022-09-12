028139ad76810852e7f31cb6f71c467113437e44
Kieran Mesquita <mesquitakieran@gmail.com>
2022-09-12 03:32:28 +0200

    Comment functions in key utils

--- src/utils/keys-utils.ts
+++ src/utils/keys-utils.ts
@@ -39,10 +39,26 @@ function verifyED25519(
 }
 
 function adjustRandom(random: string): bigint {
+  // Hash with sha256 to get a uniform random 32 bytes of data
   const randomArray = hexToBytes(sha256(random));
-  randomArray[0] &= 248;
-  randomArray[31] &= 127;
-  randomArray[31] |= 64;
+
+  // NOTE: The bits adjustment is no longer required to function as an X25519 integer is not used
+  // These steps are still taken to preserve compatibility with older transactions
+
+  // Adjust bits to match the pattern xxxxx000...01xxxxxx
+  // This ensures that the bytes are a little endian representation of an integer of the form
+  // (2^254 + 8) * x where 0 < x <= 2^251 - 1, which can be decoded as an X25519 integer
+
+  // AND operation to ensure the last 3 bits of the first byte are 0 leaving the rest unchanged
+  randomArray[0] &= 0b11111000;
+
+  // AND operation to ensure the first bit of the last byte is 0 leaving the rest unchanged
+  randomArray[31] &= 0b01111111;
+
+  // OR operation to ensure the second bit of the last byte is 0 leaving the rest unchanged
+  randomArray[31] |= 0b01000000;
+
+  // Return mod n to fit to curve point
   return BigInt(`0x${bytesToHex(randomArray)}`) % CURVE.n;
 }
 
@@ -64,7 +80,18 @@ async function getEphemeralKeys(
   random: string,
   senderBlindingKey: Optional<string>,
 ): Promise<[Uint8Array, Uint8Array]> {
-  const sharedSymmetricKey = adjustRandom(random);
+  // Adjust random value to use as blinding key to prevent external observers from being able to
+  // reverse the multiplication. The random value here is a value only known to the sender and
+  // receiver
+  const publicBlindingKey = adjustRandom(random);
+
+  // For each blinding operation both sender and receiver public viewing keys must be multiplied by
+  // the same value to preserve symmetry in relation to the respective private key to allow shared
+  // key generation
+
+  // Multiply both sender and receiver viewing public keys with the sender blinding key
+  // The sender blinding key is only known to the sender preventing the receiver from being able to
+  // invert and retrieve the original value
   const maybeBlindedSenderViewingPublicKeyPoint = maybeBlindViewingPublicKey(
     senderViewingPublicKey,
     senderBlindingKey,
@@ -73,20 +100,33 @@ async function getEphemeralKeys(
     receiverViewingPublicKey,
     senderBlindingKey,
   );
+
+  // Multiply both sender and receiver viewing public keys with the public blinding key
+  // The pub blinding key is only known to the sender and receiver preventing external
+  // observers from being able to invert and retrieve the original value
   const ephemeralKeyReceiverMaybeBlinded = maybeBlindedSenderViewingPublicKeyPoint
-    .multiply(sharedSymmetricKey)
+    .multiply(publicBlindingKey)
     .toRawBytes();
   const ephemeralKeySender = maybeBlindedReceiverViewingPublicKeyPoint
-    .multiply(sharedSymmetricKey)
+    .multiply(publicBlindingKey)
     .toRawBytes();
+
+  // Return blinded keys
   return [ephemeralKeyReceiverMaybeBlinded, ephemeralKeySender];
 }
 
 function unblindedEphemeralKey(ephemeralKey: Uint8Array, random: string): Optional<Uint8Array> {
   try {
+    // Adjust random value to recover public blinding key
     const randomAdjusted = adjustRandom(random);
+
+    // Create curve point instance from ephemeral key bytes
     const point = Point.fromHex(bytesToHex(ephemeralKey));
+
+    // Invert the scalar to undo blinding multiplication operation
     const randomInverse = utilsEd25519.invert(randomAdjusted, CURVE.n);
+
+    // Unblind by multiplying by the inverted scalar
     const unblinded = point.multiply(randomInverse);
     return unblinded.toRawBytes();
   } catch {
@@ -116,8 +156,13 @@ async function getSharedSymmetricKey(
   ephemeralKey: Uint8Array,
 ): Promise<Optional<Uint8Array>> {
   try {
+    // Create curve point instance from ephemeral key class
     const pk = Point.fromHex(bytesToHex(ephemeralKey));
+
+    // Retrieve private scalar from private key
     const { scalar } = await utilsEd25519.getExtendedPublicKey(privateKey);
+
+    // Multiply ephemeral key by private scalar to get shared key
     const symmetricKey = pk.multiply(scalar);
     return symmetricKey.toRawBytes();
   } catch (err) {
