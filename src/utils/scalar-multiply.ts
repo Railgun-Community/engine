f168b29f340587cf60006bf55e4180aa8b33f41d
Andre Staltz <andre@staltz.com>
2022-10-18 17:01:36 +0300

    replace Point.multiply with curve25519-scalarmult, fallback to javascript
    use @railgun-community/curve25519-scalarmult-wasm
    
    use correct type for curve25519-scalarmult-wasm init
    
    rename scalarmult to scalarMultiply
    
    improve awaiting init for curve25519-scalarmult-wasm
    
    Use javascript as backup for scalar multiply
    
    Fix impl
    
    call default for init function

---
+++ src/utils/scalar-multiply.ts
@@ -0,0 +1,49 @@
+import { Point } from '@noble/ed25519';
+import { bytesToHex } from 'ethereum-cryptography/utils';
+import EngineDebug from '../debugger/debugger';
+import { ByteLength, nToBytes } from './bytes';
+
+const {
+  default: initCurve25519wasm,
+  scalarMultiply: scalarMultiplyWasm,
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
+} = require('@railgun-community/curve25519-scalarmult-wasm') as {
+  default: () => Promise<void>;
+  scalarMultiply: (point: Uint8Array, scalar: Uint8Array) => Uint8Array;
+};
+
+const initCurve25519Wasm = (): Promise<void> => {
+  try {
+    // Try WASM implementation.
+    return typeof initCurve25519wasm === 'function' ? initCurve25519wasm() : Promise.resolve();
+  } catch (err) {
+    if (!(err instanceof Error)) {
+      throw err;
+    }
+    // Fallback to Javascript. No init needed.
+    EngineDebug.log('curve25519-scalarmult-wasm init failed: Fallback to JavaScript');
+    EngineDebug.error(err);
+    return Promise.resolve();
+  }
+};
+export const initCurve25519Promise = initCurve25519Wasm();
+
+export const scalarMultiplyWasmFallbackToJavascript = (
+  point: Uint8Array,
+  scalar: bigint,
+): Uint8Array => {
+  try {
+    // Try WASM implementation.
+    const scalarUint8Array = nToBytes(scalar, ByteLength.UINT_256);
+    return scalarMultiplyWasm(point, scalarUint8Array);
+  } catch (err) {
+    if (!(err instanceof Error)) {
+      throw err;
+    }
+    // Fallback to Javascript.
+    EngineDebug.log('curve25519-scalarmult-wasm scalarMultiply failed: Fallback to JavaScript');
+    EngineDebug.error(err);
+    const pk = Point.fromHex(bytesToHex(point));
+    return pk.multiply(scalar).toRawBytes();
+  }
+};
