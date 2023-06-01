97459271d2dabd07c70f7b6abd42cfd446e34606
jrmeurer <jrmeurer@wisc.edu>
2023-06-01 13:29:27 -0600

    Add deriv index to mnemonic to pkey

--- src/key-derivation/__tests__/mnemonic.test.ts
+++ src/key-derivation/__tests__/mnemonic.test.ts
@@ -13,10 +13,16 @@ describe('Key Derivation/Mnemonic', () => {
           'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about',
         seed: '1ab42cc412b618bdea3a599e3c9bae199ebf030895b039e9db1e30dafb12b727',
       },
+      {
+        mnemonic:
+          'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about',
+        seed: '413cbeb8f83ecbd3c1e64f2ed89faac0ae89b5986fb4b010422e3056bbc61174',
+        derivationIndex: 100,
+      },
     ];
 
     vectors.forEach((vector) => {
-      expect(mnemonicToPrivateKey(vector.mnemonic)).to.equal(vector.seed);
+      expect(mnemonicToPrivateKey(vector.mnemonic, vector.derivationIndex)).to.equal(vector.seed);
     });
   });
 });
--- src/key-derivation/mnemonic.ts
+++ src/key-derivation/mnemonic.ts
@@ -1,11 +1,15 @@
 import { HDKey } from 'ethereum-cryptography/hdkey';
 import { mnemonicToSeedSync } from 'ethereum-cryptography/bip39';
 import { bytesToHex } from 'ethereum-cryptography/utils';
-import { defaultPath } from 'ethers';
 
-export const mnemonicToPrivateKey = (mnemonic: string) => {
+const getPath = (index = 0) => {
+  return `m/44'/60'/0'/0/${index}`;
+};
+
+export const mnemonicToPrivateKey = (mnemonic: string, derivationIndex?: number) => {
   const seed = mnemonicToSeedSync(mnemonic);
-  const node = HDKey.fromMasterSeed(seed).derive(defaultPath);
+  const path = getPath(derivationIndex);
+  const node = HDKey.fromMasterSeed(seed).derive(path);
   const privateKey = bytesToHex(node.privateKey as Uint8Array);
   return privateKey;
 };
