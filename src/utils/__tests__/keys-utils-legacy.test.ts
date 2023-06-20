062f205526f04bf26c325a87c4f104dd9f69ac44
Andre Staltz <andre@staltz.com>
2023-06-20 14:49:09 +0300

    test getSharedSymmetricKeyLegacy

---
+++ src/utils/__tests__/keys-utils-legacy.test.ts
@@ -0,0 +1,23 @@
+import { bytesToHex } from '@noble/hashes/utils';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { getSharedSymmetricKeyLegacy } from '../keys-utils-legacy';
+import { hexStringToBytes } from '../bytes';
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
+describe('Test keys-utils-legacy', () => {
+  it('getSharedSymmetricKeyLegacy stability', async () => {
+    const privateKeyPairA = hexStringToBytes(
+      '0123456789012345678901234567890123456789012345678901234567891234',
+    );
+    const blindedPublicKeyPairB = hexStringToBytes(
+      '0987654321098765432109876543210987654321098765432109876543210987',
+    );
+    const symmetricKey = await getSharedSymmetricKeyLegacy(privateKeyPairA, blindedPublicKeyPairB);
+    expect(bytesToHex(symmetricKey as Uint8Array)).to.equal(
+      '64df806eadf85aba48fe2a7b4ecc783affcbeb7f63e9c9f6b8eb9422e2322874',
+    );
+  });
+});
