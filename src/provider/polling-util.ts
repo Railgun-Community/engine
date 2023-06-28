00f7ed8405afc2baa1f97a75dbbf96b416d69b1e
jrmeurer <jrmeurer@wisc.edu>
2023-06-28 10:53:29 -0500

    Update fallback provider polling util for general use w/ ethers versions

--- src/provider/polling-util.ts
+++ src/provider/polling-util.ts
@@ -30,30 +30,31 @@ export const createPollingJsonRpcProviderForListeners = async (
     return provider;
   }
 
-  if (provider instanceof JsonRpcProvider) {
+  if ('pollingInterval' in provider) {
     // eslint-disable-next-line no-underscore-dangle
     const { url } = provider._getConnection();
     const { chainId } = await provider.getNetwork();
     return new PollingJsonRpcProvider(url, Number(chainId));
   }
 
-  if (provider instanceof FallbackProvider) {
+  if ('quorum' in provider) {
+    // FallbackProvider only
+
     if (!provider.providerConfigs.length) {
       throw new Error('Requires 1+ providers in FallbackProvider');
     }
-
-    // TODO: Support WSS providers for polling.
-    if (!(provider.providerConfigs[0].provider instanceof JsonRpcProvider)) {
+    const firstProvider = provider.providerConfigs[0].provider as JsonRpcProvider;
+    if (!('pollingInterval' in firstProvider)) {
       throw new Error('First provider in FallbackProvider must be JsonRpcProvider');
     }
 
     // eslint-disable-next-line no-underscore-dangle
-    const { url } = provider.providerConfigs[0].provider._getConnection();
+    const { url } = firstProvider._getConnection();
     const { chainId } = await provider.getNetwork();
     // eslint-disable-next-line no-underscore-dangle
-    const maxLogsPerBatch = provider.providerConfigs[0].provider._getOption('batchMaxCount');
+    const maxLogsPerBatch = firstProvider._getOption('batchMaxCount');
     return new PollingJsonRpcProvider(url, Number(chainId), pollingInterval, maxLogsPerBatch);
   }
 
-  throw new Error('Invalid provider type - make sure you are using the same version of ethers');
+  throw new Error('Invalid ethers provider type');
 };
