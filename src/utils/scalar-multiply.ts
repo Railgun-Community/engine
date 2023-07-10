3236e3be6eaeeee5092afce07d8f5687cdf37c44
André Staltz <andre@staltz.com>
2023-07-10 18:10:01 +0300

    Revert "revert" scalarmult changes (#42)
    * Use wasm scalarmult in getSharedSymmetricKeyLegacy
    
    * scalar-multiply: always fallback to JS in RN environment (#40)
    
    * scalar-multiply: avoid calling Noble when y coord invalid
    
    * use isReactNative from utils/runtime
    
    * add tests for scalarMultiply tests

--- src/utils/scalar-multiply.ts
+++ src/utils/scalar-multiply.ts
@@ -2,15 +2,16 @@ import { Point } from '@noble/ed25519';
 import { bytesToHex } from 'ethereum-cryptography/utils';
 import EngineDebug from '../debugger/debugger';
 import { ByteLength, nToBytes } from './bytes';
+import { isReactNative } from './runtime';
 
-const {
-  default: initCurve25519wasm,
-  scalarMultiply: scalarMultiplyWasm,
+interface ScalarMultMod {
+  default?: () => Promise<void>;
+  scalarMultiply?: (point: Uint8Array, scalar: Uint8Array) => Uint8Array;
+}
+
+const { default: initCurve25519wasm, scalarMultiply: scalarMultiplyWasm } =
   // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
-} = require('@railgun-community/curve25519-scalarmult-wasm') as {
-  default: () => Promise<void>;
-  scalarMultiply: (point: Uint8Array, scalar: Uint8Array) => Uint8Array;
-};
+  (isReactNative ? {} : require('@railgun-community/curve25519-scalarmult-wasm')) as ScalarMultMod;
 
 const initCurve25519Wasm = (): Promise<void> => {
   try {
@@ -32,12 +33,20 @@ export const scalarMultiplyWasmFallbackToJavascript = (
   point: Uint8Array,
   scalar: bigint,
 ): Uint8Array => {
+  if (isReactNative || !scalarMultiplyWasm) {
+    // Fallback to JavaScript if this module is running directly in React Native
+    return scalarMultiplyJavascript(point, scalar);
+  }
   try {
     // Try WASM implementation.
     const scalarUint8Array = nToBytes(scalar, ByteLength.UINT_256);
     return scalarMultiplyWasm(point, scalarUint8Array);
   } catch (err) {
     if (!(err instanceof Error)) {
+      throw err;
+    }
+    if (err.message.includes('invalid y coordinate')) {
+      // Noble/ed25519 would also throw this error, so no need to call Noble
       throw err;
     }
     // Fallback to Javascript.
