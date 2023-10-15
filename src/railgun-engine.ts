9e990c5722cd023bdb6636921fc69768b027ddd0
jrmeurer <jrmeurer@wisc.edu>
2023-10-15 17:32:47 +0200

    Update

--- src/railgun-engine.ts
+++ src/railgun-engine.ts
@@ -895,7 +895,9 @@ class RailgunEngine extends EventEmitter {
 
       if (missingAnyCommitments) {
         EngineDebug.error(
-          new Error(`Missing a commitment. Cannot continue queuing Railgun TXIDs.`),
+          new Error(
+            `Missing a commitment. Stopping queue of Railgun TXIDs. This will occur if the TXIDs are further than the UTXOs data source.`,
+          ),
         );
         break;
       }
