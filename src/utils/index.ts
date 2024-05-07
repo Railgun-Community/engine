c9f180585c90d2d822095532b24e2cedd456f19e
Andre Staltz <andre@staltz.com>
2024-05-07 13:04:29 +1000

    Refactor: remove ciphertext utils as public API

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 // Note: we purposefully do not export everything, in order to reduce the number of public APIs
 export { ByteLength, ByteUtils } from './bytes';
-export * from './encryption/ciphertext';
 export { convertTransactionStructToCommitmentSummary } from './commitment';
 export * from './ecies';
 export { getPublicViewingKey, verifyED25519 } from './keys-utils';
