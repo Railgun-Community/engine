be83b21320cccab7808c3229281dc5dfe47e80bb
zy0n.bear <zy0n@users.noreply.github.com>
2024-05-15 16:07:18 -0400

    Zy0n/db export (#98)
    * export database from engine
    
    * expose fromUTF8String and toUTF8String as public

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 // Note: we purposefully do not export everything, in order to reduce the number of public APIs
-export { ByteLength, ByteUtils } from './bytes';
+export { ByteLength, ByteUtils, fromUTF8String, toUTF8String } from './bytes';
 export { convertTransactionStructToCommitmentSummary } from './commitment';
 export * from './ecies';
 export { getPublicViewingKey, verifyED25519 } from './keys-utils';
