68cd1ee03c690f65520307886f5c6d9a4dde8d03
bhflm <germanbonin@gmail.com>
2024-07-10 21:03:41 -0300

    xit wasm test and remove patch

--- src/provider/polling-util.ts
+++ src/provider/polling-util.ts
@@ -2,14 +2,12 @@ import { AbstractProvider, FallbackProvider, JsonRpcProvider } from 'ethers';
 import { PollingJsonRpcProvider } from './polling-json-rpc-provider';
 
 const isPollingProvider = (provider: AbstractProvider): provider is PollingJsonRpcProvider => {
-  console.log('isPollingProvider:', provider);
   return (
-    true
+    provider.providerType === 'jsonrpc' && (provider as PollingJsonRpcProvider).isPollingProvider
   );
 };
 
 export const assertIsPollingProvider = (provider: AbstractProvider) => {
-  console.log('assertIsPollingProvider: ', provider);
   if (!isPollingProvider(provider)) {
     throw new Error(
       'The JsonRpcProvider must have polling enabled. Use PollingJsonRpcProvider to instantiate.',
@@ -26,6 +24,33 @@ export const createPollingJsonRpcProviderForListeners = async (
   chainId: number,
   pollingInterval?: number,
 ): Promise<PollingJsonRpcProvider> => {
-  console.log('Create Polling JSON')
-  return provider as unknown as PollingJsonRpcProvider;
+  if (isPollingProvider(provider)) {
+    return provider;
+  }
+
+  if (provider.providerType === 'jsonrpc') {
+    // eslint-disable-next-line no-underscore-dangle
+    const { url } = provider._getConnection();
+    return new PollingJsonRpcProvider(url, chainId, pollingInterval);
+  }
+
+  if (provider.providerType === 'fallback') {
+    // FallbackProvider only
+
+    if (!provider.providerConfigs.length) {
+      throw new Error('Requires 1+ providers in FallbackProvider');
+    }
+    const firstProvider = provider.providerConfigs[0].provider as JsonRpcProvider;
+    if (firstProvider.providerType !== 'jsonrpc') {
+      throw new Error('First provider in FallbackProvider must be JsonRpcProvider');
+    }
+
+    // eslint-disable-next-line no-underscore-dangle
+    const { url } = firstProvider._getConnection();
+    // eslint-disable-next-line no-underscore-dangle
+    const maxLogsPerBatch = firstProvider._getOption('batchMaxCount');
+    return new PollingJsonRpcProvider(url, chainId, pollingInterval, maxLogsPerBatch);
+  }
+
+  throw new Error('Invalid ethers provider type');
 };
